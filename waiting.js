2393aab9a0020198d0b3cb520af843d205f05bbf
haid0109 <51113567+haid0109@users.noreply.github.com>
2020-02-10 15:26:02 +0100

    combined waiting.js and waitingRoom.js

--- waiting.js
+++ waiting.js
@@ -1,28 +1,23 @@
-window.addEventListener("onload", onload = async function () {
-
-    await fetch('http://localhost:9423/game/current')
-        .then((response) => {
-            return response.json();
-            
-        })
-        .then((game) => {
-            let players = game.players;
-
-            var elements = players.map(player => `<div class="player-wrapper">
-                                                        <img src = "" alt = "">
-                                                        <div class="player-text">
-                                                            <p>player 1</p>
-                                                            <p>${player.name}</p>
-                                                        </div>                       
-                                                    </div >`    
-            ).join("");
-
-            document.getElementById("col-1").innerHTML = elements;
-
-            
+function displayPlayersInColumn1(){
+    fetch('http://localhost:9423/game/current')
+    .then((resp) => {return resp.json()})
+    .then((game) => {
+        let players = game.players;
+        var elements = players.map(player =>   `<div class="player-wrapper">
+                                                    <img src = "" alt = "">
+                                                    <div class="player-text">
+                                                        <p>player 1</p>
+                                                        <p>${player.name}</p>
+                                                    </div>                       
+                                                </div >`    
+        ).join("");
+        document.getElementById("col-1").innerHTML = elements;
     })
-        .catch((error) => { console.error('Error:', error); });
+    .catch((error) => { console.error('Error:', error); });
+}
 
+window.addEventListener("load", displayPlayersInColumn1);
 
-
+window.addEventListener("load", function(){
+    setInterval(displayPlayersInColumn1, 5000);
 });
