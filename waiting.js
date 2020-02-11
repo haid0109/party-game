cb1f38d051b838ba0cee9530677f67db1b5739f4
haid0109 <51113567+haid0109@users.noreply.github.com>
2020-02-11 12:09:45 +0100

    fixed "player 1" issue

--- waiting.js
+++ waiting.js
@@ -2,16 +2,20 @@ function displayPlayersInColumn1(){
     fetch('http://localhost:9423/game/current')
     .then((resp) => {return resp.json()})
     .then((game) => {
+        let playerCount = 1;
         let players = game.players;
-        var elements = players.map(player =>   `<div class="player-wrapper">
-                                                    <img src = "" alt = "">
-                                                    <div class="player-text">
-                                                        <p>player 1</p>
-                                                        <p>${player.name}</p>
-                                                    </div>                       
-                                                </div >`    
+        var elements = players.map(player => {
+            return `<div class="player-wrapper">
+                <img src = "" alt = "">
+                <div class="player-text">
+                    <p>player ${playerCount++}</p>
+                    <p>${player.name}</p>
+                </div>                       
+            </div >`;
+        }    
         ).join("");
         document.getElementById("col-1").innerHTML = elements;
+        console.log(elements);
     })
     .catch((error) => { console.error('Error:', error); });
 }
