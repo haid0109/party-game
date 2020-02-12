000a69a0e94a29a5d41a19da28863f2c1c2c751e
haid0109 <51113567+haid0109@users.noreply.github.com>
2020-02-12 16:12:45 +0100

    Update upload.js

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -4,7 +4,8 @@ window.addEventListener("load", () => {
           navigator.mediaDevices.getUserMedia);
       }
       if (hasGetUserMedia()) {
-          
+          const contraints = {video: true};
+          const video = document
       } 
       else { alert('getUserMedia() is not supported by your browser'); }
 })
