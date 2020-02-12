025684cb58688677561a6dc79ad81507f62a50e6
haid0109 <51113567+haid0109@users.noreply.github.com>
2020-02-12 14:36:53 +0100

    added audio and authentication
    added audio tag and authentication for  Audio Web API

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -3,9 +3,8 @@ window.addEventListener("load", () => {
         return !!(navigator.mediaDevices &&
           navigator.mediaDevices.getUserMedia);
       }
-
       if (hasGetUserMedia()) {
-      } else {
-        alert('getUserMedia() is not supported by your browser');
-      }
+          
+      } 
+      else { alert('getUserMedia() is not supported by your browser'); }
 })
