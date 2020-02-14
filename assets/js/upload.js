8cd3fcfaf091339e702ad5080a47cff99370f910
haid0109 <51113567+haid0109@users.noreply.github.com>
2020-02-14 13:59:19 +0100

    added prompt to use audio

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,11 +1,16 @@
 window.addEventListener("load", () => {
     function hasGetUserMedia() {
-        return !!(navigator.mediaDevices &&
-          navigator.mediaDevices.getUserMedia);
-      }
-      if (hasGetUserMedia()) {
-          const contraints = {video: true};
-          const video = document
-      } 
-      else { alert('getUserMedia() is not supported by your browser'); }
+        return !!(navigator.mediaDevices.getUserMedia);
+    }
+    if (hasGetUserMedia()) {
+        // const player = document.getElementById('player');
+        // const handleSuccess = function(stream) {
+        //     if (window.URL) { player.srcObject = stream; } 
+        //     else { player.src = stream; }
+        // };
+        // navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+        // .then(handleSuccess);
+        
+    } 
+    else { alert('getUserMedia() is not supported by your browser'); }
 })
