64bf15db2fe9bc3a4567d8364471e2bce5ade040
itami187 <alexandra@deranged.dk>
2020-03-04 12:54:04 +0100

    added javascript to write out player list for the awaitingAnswers.html page

---
+++ assets/js/awaitingAnswers.js
@@ -0,0 +1,69 @@
+async function displayPlayersInColumn1(){
+    await fetch('http://localhost:9423/game/current')
+    .then((resp) => {return resp.json()})
+    .then((game) => {
+        let playerCount = 1;
+        let players = game.players;
+        var elements = players.map(player => {
+            if(player.playerReady == true){
+                return `<div class="player-wrapper">
+                <img src = "" alt = "">
+                <div class="player-text">
+                    <p>player ${playerCount++}</p>
+                    <p>${player.name}</p>
+                </div>
+                <div class="theirGuess">
+                    <p>Guess</p>
+                    <p></p>
+                </div>
+                <div class="ready-wrapper">
+                    <div id="ready-marker" style="background-color: red;"></div>
+                </div>                       
+            </div >`;   
+            }
+
+            if(player.playerReady == false){
+                return `<div class="player-wrapper">
+                <img src = "" alt = "">
+                <div class="player-text">
+                    <p>player ${playerCount++}</p>
+                    <p>${player.name}</p>
+                </div>
+                <div class="theirGuess">
+                    <p>Guess</p>
+                    <p></p>
+                </div>
+                <div class="ready-wrapper">
+                    <div id="ready-marker" style="background-color: green;"></div>
+                </div>                       
+            </div >`;   
+            }
+
+            if(!player.playerReady){
+                return `<div class="player-wrapper">
+                <img src = "" alt = "">
+                <div class="player-text">
+                    <p>player ${playerCount++}</p>
+                    <p>${player.name}</p>
+                </div>
+                <div class="theirGuess">
+                    <p>Guess</p>
+                    <p></p>
+                </div>
+                <div class="ready-wrapper">
+                    <div id="ready-marker" style="background-color: red;"></div>
+                </div>                       
+            </div >`; 
+            }
+            
+        }).join("");
+        document.getElementById("col-1").innerHTML = elements;
+    })
+    .catch((error) => { console.error('Error:', error); });
+}
+
+
+
+window.addEventListener("load", () => displayPlayersInColumn1(), false);
+window.addEventListener("load", () => setInterval(displayPlayersInColumn1, 5000));
+
